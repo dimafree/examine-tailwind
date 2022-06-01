b3169eeb4dd62ba32d985f477f930c1abf1eb312
dimafree <dimepifann@gmail.com>
2022-06-01 07:51:27 -0700

    fix: add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
   const password = document.getElementsByClassName('password')[0];
 
-  password.addEventListener('click', function (e) {
-    const input = e.target.parentElement.querySelector('input');
-    if (input.getAttribute('type') === 'text') {
-      input.setAttribute('type', 'password');
-    } else {
-      input.setAttribute('type', 'text');
-    }
+  // show password
+  if (password) {
+    password.addEventListener('click', function (e) {
+      const input = e.target.parentElement.querySelector('input');
+      if (input.getAttribute('type') === 'text') {
+        input.setAttribute('type', 'password');
+      } else {
+        input.setAttribute('type', 'text');
+      }
+    });
+  }
+
+  // open header menu
+  const menu = document.getElementsByClassName('menu')[0];
+  const burger = document.getElementsByClassName('burger-js')[0];
+  const closeBtn = document.getElementsByClassName('close-js')[0];
+  const mobileMenu = document.getElementsByClassName('mobile-menu-js')[0];
+  const burgerMobile = document.getElementsByClassName('burger-mobile-js')[0];
+
+  burger.addEventListener('click', function () {
+    menu.classList.toggle('show')
+    burger.classList.toggle('show')
+  });
+  burgerMobile.addEventListener('click', function () {
+    mobileMenu.classList.toggle('show')
+  });
+
+  closeBtn.addEventListener('click', function () {
+    mobileMenu.classList.remove('show')
+    burger.classList.remove('show')
+    menu.classList.remove('show')
   });
 });
